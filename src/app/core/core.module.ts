e295843ba1f12dd1b08a4ec7bdff1edc87670a1f
Vinicius Colasanto <vscolasanto@gmail.com>
2021-08-18 21:14:18 -0300

    feat: add navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { LOCALE_ID, NgModule } from '@angular/core'
 import { CommonModule, registerLocaleData } from '@angular/common'
 import localePT from '@angular/common/locales/pt'
+import { RouterModule } from '@angular/router'
 
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -8,16 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 
 import { NgBrazil } from 'ng-brazil'
-import { TextMaskModule } from 'angular2-text-mask'
+import { TextMaskModule } from 'angular2-text-mask';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 registerLocaleData(localePT)
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
     NgBrazil,
     TextMaskModule,
@@ -25,7 +30,8 @@ registerLocaleData(localePT)
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ],
   providers: [
     { provide: LOCALE_ID, useValue: "pt" }
