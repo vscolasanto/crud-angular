5da36b9da4cf21adff1e75fe040ef79d2f0bc64b
Vinicius Colasanto <vscolasanto@gmail.com>
2021-08-18 22:48:03 -0300

    refactor: change navbar styles, add author and books module to global router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'authors',
+    loadChildren: () => import('./pages/authors/authors.module')
+      .then((m) => m.AuthorsModule)
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./pages/books/books.module')
+      .then((m) => m.BooksModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
