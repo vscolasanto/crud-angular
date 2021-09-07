ed93dec98526ba98f9e5eca352f78435346969af
Vinicius Colasanto <vscolasanto@gmail.com>
2021-09-07 17:04:29 -0300

    feat: update app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module')
+      .then((m) => m.HomeModule)
+  },
+  {
     path: 'authors',
     loadChildren: () => import('./pages/authors/authors.module')
       .then((m) => m.AuthorsModule)
@@ -11,6 +16,15 @@ const routes: Routes = [
     path: 'books',
     loadChildren: () => import('./pages/books/books.module')
       .then((m) => m.BooksModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./pages/categories/categories.module')
+      .then((m) => m.CategoriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/'
   }
 ]
 
