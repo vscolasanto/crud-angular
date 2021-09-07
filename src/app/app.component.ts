ce28c8f581f6ec1103d587b555fbc58ef1fdbe14
Vinicius Colasanto <vscolasanto@gmail.com>
2021-09-07 16:15:15 -0300

    feat: add pimeng calendar pt-br configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { PrimeNGConfig } from 'primeng/api'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
 
-  constructor() { }
+  constructor(private config: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.config.setTranslation({
+      dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+      dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+      dayNamesMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+      monthNames: [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+      ],
+      monthNamesShort: [
+        "Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+        "Jul", "Ago","Set", "Out", "Nov", "Dez"
+      ],
+      today: "Hoje",
+      clear: "Limpar",
+      dateFormat: "dd/MM/yyyy",
+      weekHeader: "Sem"
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
 import { CoreModule } from './core/core.module'
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module'
   imports: [
     CoreModule,
     AppRoutingModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
