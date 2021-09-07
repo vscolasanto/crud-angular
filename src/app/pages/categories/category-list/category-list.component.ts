4f509002048133ab6edf3beb24efac73426e89fd
Vinicius Colasanto <vscolasanto@gmail.com>
2021-09-07 17:01:48 -0300

    feat: add category list component

---
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
+import { Category } from '../models/category.model';
+import { CategoriesService } from '../services/categories.service';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
+  constructor(
+    private categoriesService: CategoriesService
+  ) {
+    super(categoriesService)
+  }
+
+  protected removeError() {
+    return 'Erro ao remover Categoria.'
+  }
+
+  protected listError() {
+    return 'Erro ao carregar listagem de categorias.'
+  }
+}
